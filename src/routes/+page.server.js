773b3224a9fe29e2c4bcb4f43d0a3e5ce1be68bc
Jynx <thekillercraftlp@gmail.com>
2023-05-24 22:40:51 +0200

    - fetched characters names - added pages for characters (WIP) - added nations

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,13 +1,52 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load({}) {
+    /* Fetch characterlist */
     let response = await fetch(`https://api.genshin.dev/characters/`);
-    const characters = await response.json();
+    const characterlist = await response.json();
 
+    /* Fetch name of each character */
+    let characters = [];
+
+
+
+    for (let i = 0; i < characterlist.length; i++) {
+        let key = characterlist[i];
+        response = await fetch(`https://api.genshin.dev/characters/${key}`);
+        let data = await response.json();
+        let name = undefined;
+
+        if (data.name) {
+            name = data.name;
+        }
+
+        let img = `https://api.genshin.dev/characters/${key}/icon`;
+        
+        await characters.push({key, name, img})
+    }
+    
+    // characterlist.forEach(async key => {
+    //     response = await fetch(`https://api.genshin.dev/characters/${key}`);
+    //     let data = await response.json();
+    //     let name = undefined;
+
+    //     if (data.name) {
+    //         name = data.name;
+    //     }
+
+    //     let img = `https://api.genshin.dev/characters/${key}/icon`
+
+    //     await characters.push({key, name, img})
+    // });
+    
     response = await fetch(`https://api.genshin.dev/elements/`);
     const elements = await response.json();
-    
+
+    response = await fetch(`https://api.genshin.dev/nations`);
+    const nations = await response.json();
+
     return {
         characters: characters,
         elements: elements,
+        nations: nations,
     }
 }
